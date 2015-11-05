a86bc963cd5ddbd9a521448858f20ff7d8bfd280
Alexi Maschas <alexi.maschas@gmail.com>
2015-11-05 03:14:50 -0500

    adding custom post type category archives

--- functions.php
+++ functions.php
@@ -11,3 +11,14 @@ wp_enqueue_script( 'global-js', get_stylesheet_directory_uri() . '/static/js/glo
 add_image_size( 'carousel-thumb', 930, 450, true );
 add_image_size( 'large-thumb', 500, 191, true );
 add_image_size( 'med-thumb', 335, 130, true );
+
+function ahny_category_cpt_rewrites() {
+    $custom_post_types = get_post_types();
+    $custom_post_types[] = 'property';
+    foreach ( $custom_post_types as $post_type ) {
+        $rule = '^' . $post_type . '/category/(.+?)/?$';
+        $rewrite = 'index.php?post_type=' . $post_type . '&category_name=$matches[1]';
+        add_rewrite_rule($rule,$rewrite,'top');
+    }
+}
+add_action('init', 'ahny_category_cpt_rewrites');
