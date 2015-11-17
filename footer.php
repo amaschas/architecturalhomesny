bc3027dee75ae3cb76948a9a9edeee26752358a8
Alexi Maschas <alexi.maschas@gmail.com>
2015-11-16 22:08:42 -0500

    adding footer logo

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-					<footer id="main-footer"><p>Todd Goddard <b>Broker Associate, EcoBroker&reg;</b> M: 914.357.2776  T: 877.304.3662 <a href="mailto:tgoddard@houlihanlawrence.com">tgoddard@houlihanlawrence.com</a></p></footer>
+						<footer id="main-footer"><p>Todd Goddard <b>Broker Associate, EcoBroker&reg;</b> M: 914.357.2776  T: 877.304.3662 <a href="mailto:tgoddard@houlihanlawrence.com">tgoddard@houlihanlawrence.com</a></p></footer>
+					<?php if ( is_home() ) : ?>
+						<span id="footer-logo"></span>
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
